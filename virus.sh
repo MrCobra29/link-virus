eee5e52234bb51a23098033a9f9555751be2a496
MrCobra29 <92773555+MrCobra29@users.noreply.github.com>
2021-10-19 13:19:46 +0700

    Add files via upload

---
+++ virus.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+# By Mr Cobra29
+
+# variabel
+e=echo
+s=sleep
+v=https
+z=bit.ly
+k=3ild93al
+
+# pembersih teks
+c=clear
+$c # pembersih
+
+# tampilan
+$s 2
+figlet virus
+$s 1
+$e "=================================================="
+$e " {•} Author : Mr Cobra 29"
+$e " {•} Team   : Mafia Teknologi"
+$e "=================================================="
+$s 1
+# daftar menu
+echo "        <•> Welcome User Termux <•>"
+echo "================================================"
+echo "{1} virus"
+echo "{×} Keluar"
+echo "================================================"
+read -p "{•} Pilih: " option;
+# data base
+if [[ $option == "1" ]]
+then
+    echo
+    echo "sabar........."
+    sleep 5
+    $e "         ↓↓↓↓"
+    $e $v://$z/$k
+    echo "÷÷÷÷÷÷÷÷÷÷÷÷"
+else
